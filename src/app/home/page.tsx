4d75a278101d089d40befdea4b74b5116e0ce730
simran201 <simran@revinfotech.com>
2024-01-08 12:57:33 +0530

    initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push("/login");
+    }
+  }, []);
+  return (
+    <>
+    <div className="main-page">
+      <h1 className="main-title">You Are Suceesfully Logged In</h1>
+      <h3 className="subtitle"> This is Your Home Page</h3>
+      <button
+        onClick={() => {
+          localStorage.removeItem('token')
+          console.log('logout');
+          window.location.href = "/login";
+
+        }}
+      >
+        Logout
+      </button>
+    </div>
+    </>
+  );
+}
